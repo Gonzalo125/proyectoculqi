65213d40e4aef3d24404d4e43fd004d8b530a42a
GonzaloSO <gonproyect2023@gmail.com>
2023-11-12 10:01:49 -0500

    Add docker compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,23 @@
-import { Module } from '@nestjs/common';
+import {  Module } from '@nestjs/common';
+import * as redisStore from 'cache-manager-redis-store';
 import { CacheModule } from '@nestjs/cache-manager';
 import { AuthModule } from './auth/auth.module';
-import * as redisStore from 'cache-manager-redis-store';
 
-
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     CacheModule.register({
       isGlobal: true,
       store: redisStore,
-      host: 'localhost',
+      host: "172.28.0.2",
+      username:"root",
       port: 6379,
-      ttl: 60
-      
+      ttl: 120
     }),
     AuthModule,
   ],
- 
 })
 
 export class AppModule {}
